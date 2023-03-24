9775c191f5a8061643faf564a4943469afe27998
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-03-24 23:57:55 +0900

    feat : 3.1 Named Parameters

--- hi.dart
+++ hi.dart
@@ -1,7 +1,14 @@
-String sayHello(String name) => 'Hello $name';
-
-num plus(num a, num b) => a + b;
+String sayHello({
+  required String name,
+  required int age,
+  required String country,
+}) {
+  return "hello $name, you are $age years old, from $country";
+}
 
 void main() {
-  print(sayHello("chan"));
+  print(sayHello(
+    age: 12,
+    name: "John",
+  ));
 }
