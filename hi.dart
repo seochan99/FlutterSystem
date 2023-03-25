56cc065ebb706ef4b0a677e27c01e0b62da436d9
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-03-25 10:33:05 +0900

    feat : 4.5 Cascade Notation

--- hi.dart
+++ hi.dart
@@ -1,30 +1,13 @@
 class Player {
-  final String name;
-  int xp, age;
+  String name;
+  int xp;
   String team;
 
   Player({
     required this.name,
     required this.xp,
     required this.team,
-    required this.age,
   });
-
-  Player.createBluePlayer({
-    required String name,
-    required int age,
-  })  : this.age = age,
-        this.name = name,
-        this.team = "blue",
-        this.xp = 0;
-
-  Player.createRedPlayer(String name, int age)
-      : this(
-          name: name,
-          age: age,
-          team: "red",
-          xp: 0,
-        );
 
   void sayHello() {
     print("Hello, my name is $name");
@@ -32,15 +15,10 @@ class Player {
 }
 
 void main() {
-  var player = Player.createBluePlayer(
-    name: "max",
-    age: 20,
-  );
-  player.sayHello();
-
-  var redPlayer = Player.createRedPlayer(
-    "lucy",
-    30,
-  );
-  redPlayer.sayHello();
+  var chan = Player(name: 'chan', xp: 1200, team: 'red')
+  var seo = chan
+    ..name = "nico"
+    ..xp = 1000
+    ..team = "blue"
+    ..sayHello();
 }
