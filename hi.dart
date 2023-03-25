c34d87e0157a2d95dff3dfed8b82c92aa687f2f1
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-03-25 09:50:28 +0900

    feat : 4.2 Named Constructor Parameters

--- hi.dart
+++ hi.dart
@@ -1,17 +1,35 @@
 class Player {
   final String name;
   int xp;
+  String team;
+  int age;
 
-  Player(this.name, this.xp);
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
+
   void sayHello() {
     print("Hello, my name is $name");
   }
 }
 
 void main() {
-  var player1 = Player("Chan", 100);
+  var player1 = Player(
+    name: "max",
+    xp: 100,
+    team: "red",
+    age: 20,
+  );
   player1.sayHello();
 
-  var player2 = Player("min", 120);
+  var player2 = Player(
+    name: "lucy",
+    xp: 200,
+    team: "blue",
+    age: 30,
+  );
   player2.sayHello();
 }
