5e07fc68e1a05b1c1fc49a8482b0f94123a6c6d9
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-04-01 01:02:29 +0900

    feat : 3.2 Buttons Section

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -19,6 +19,7 @@ class App extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
@@ -45,6 +46,57 @@ class App extends StatelessWidget {
                         ),
                       ),
                     ],
+                  )
+                ],
+              ),
+              // 두번째 자식
+              SizedBox(
+                height: 120,
+              ),
+              // 세번째 자식
+              Text(
+                "Total Balance",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              // 4번째 자식
+              SizedBox(
+                height: 5,
+              ),
+              // 5번째 자식
+              Text(
+                "\$5 194 482",
+                style: TextStyle(
+                  fontSize: 48,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              // 6번째 자식
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text(
+                        "Transfer",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
                   )
                 ],
               )
