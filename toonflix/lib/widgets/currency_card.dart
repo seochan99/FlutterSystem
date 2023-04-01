4f27b52a6598a9418741671deacd5890c91fed6e
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-04-01 12:40:08 +0900

    feat : 3.9 Code Challenge

--- toonflix/lib/widgets/currency_card.dart
+++ toonflix/lib/widgets/currency_card.dart
@@ -4,7 +4,9 @@ class CurrencyCard extends StatelessWidget {
   final String name, code, amount;
   final IconData icon;
   final bool isInverted;
+  final double gap;
 
+  // const : 상수
 // 검정색
 // _ : private
   final _blackColor = const Color(0xFF1F2123);
@@ -16,68 +18,72 @@ class CurrencyCard extends StatelessWidget {
     required this.amount,
     required this.icon,
     required this.isInverted,
+    required this.gap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // overflow 됐을때 어떻게 하는지 설정하는 ClipBehavior
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: isInverted ? Colors.white : _blackColor,
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: Padding(
-        // padding
-        padding: const EdgeInsets.all(30),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Text(
-                name,
-                style: TextStyle(
-                  color: isInverted ? _blackColor : Colors.white,
-                  fontSize: 32,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Row(
-                children: [
-                  Text(
-                    amount,
-                    style: TextStyle(
-                      color: isInverted ? _blackColor : Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Text(code,
-                      style: TextStyle(
-                        color: isInverted
-                            ? _blackColor
-                            : Colors.white.withOpacity(0.8),
-                        fontSize: 20,
-                      )),
-                ],
-              )
-            ]),
-            Transform.scale(
-                scale: 2.2,
-                child: Transform.translate(
-                  offset: const Offset(-5, 12),
-                  child: Icon(
-                    icon,
+    return Transform.translate(
+      offset: Offset(0, gap),
+      child: Container(
+        // overflow 됐을때 어떻게 하는지 설정하는 ClipBehavior
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: isInverted ? Colors.white : _blackColor,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Padding(
+          // padding
+          padding: const EdgeInsets.all(30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Text(
+                  name,
+                  style: TextStyle(
                     color: isInverted ? _blackColor : Colors.white,
-                    size: 88,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w600,
                   ),
-                ))
-          ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      amount,
+                      style: TextStyle(
+                        color: isInverted ? _blackColor : Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(code,
+                        style: TextStyle(
+                          color: isInverted
+                              ? _blackColor
+                              : Colors.white.withOpacity(0.8),
+                          fontSize: 20,
+                        )),
+                  ],
+                )
+              ]),
+              Transform.scale(
+                  scale: 2.2,
+                  child: Transform.translate(
+                    offset: const Offset(-5, 12),
+                    child: Icon(
+                      icon,
+                      color: isInverted ? _blackColor : Colors.white,
+                      size: 88,
+                    ),
+                  ))
+            ],
+          ),
         ),
       ),
     );
