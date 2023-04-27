628d36e46284be19571921656e800885c4b84f83
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-04-27 16:46:15 +0900

    feat : 6.8 Webtoon Card

--- webtoonflix/lib/screens/home_screen.dart
+++ webtoonflix/lib/screens/home_screen.dart
@@ -29,17 +29,17 @@ class HomeScreen extends StatelessWidget {
           // 스냅샵 데이터가 있는지 확인
           if (snapshot.hasData) {
             // 많은 양 데이터 보여주고 싶을때 ListView사용
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              // separtorBuilder: 구분자를 만들어주는 위젯
-              separatorBuilder: (context, index) {
-                return const SizedBox(width: 10);
-              },
+            // listView길이를 몰라서 에러가 발생함
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                //expanded: 위젯이 차지할 수 있는 최대 영역을 차지하도록 함
+                Expanded(
+                  child: makeList(snapshot),
+                ),
+              ],
             );
           }
           // 스냅샵 데이터가 없으면 아래 출력
@@ -49,6 +49,60 @@ class HomeScreen extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            //image
+            // container를 사용하는 이유는 decoration을 사용할 예정이기 때문
+            Container(
+              width: 250,
+              // clipBehavior: 이미지를 컨테이너에 맞게 잘라줌
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                // borderRadius: 컨테이너의 모서리를 둥글게 만들어줌
+                borderRadius: BorderRadius.circular(10),
+                // boxShadow: 그림자 효과
+                boxShadow: [
+                  BoxShadow(
+                    // color: 그림자 색상
+                    color: Colors.grey.withOpacity(0.5),
+                    blurRadius: 15,
+                    // offset: 그림자 위치
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Image.network(webtoon.thumb),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        );
+      },
+      // separtorBuilder: 구분자를 만들어주는 위젯
+      separatorBuilder: (context, index) {
+        return const SizedBox(width: 40);
+      },
     );
   }
 }
